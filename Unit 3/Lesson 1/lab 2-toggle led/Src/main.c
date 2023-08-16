5f5a69d6557c0ee9dabd0af982c76d3975440d0f
mohamed elatar <139291969+mohamed-elatar@users.noreply.github.com>
2023-08-17 02:32:04 +0300

    Update Stm32f103c6

--- Unit 3/Lesson 1/lab 2-toggle led/Src/main.c
+++ Unit 3/Lesson 1/lab 2-toggle led/Src/main.c
@@ -21,7 +21,7 @@ typedef volatile uint32_t 	vuint32_t;
 #define RCC_BASE 			0x40021000
 #define PORTA_BASE 			0x40010800
 
-#define RCC_APB2ENR 		*((volatile uint32_t*)(RCC_BASE + 0x18))
+#define RCC_APB2ENR			*((volatile uint32_t*)(RCC_BASE + 0x18))
 #define GPIOA_CRH 			*((volatile uint32_t*)(PORTA_BASE + 0x04))
 #define GPIOA_ODR 			*((volatile uint32_t*)(PORTA_BASE + 0x0C))
 
@@ -42,7 +42,7 @@ int main(void)
 {
 	RCC_APB2ENR |= (1<<2);
 	GPIOA_CRH 	&= (0XFF0FFFFF);
-	GPIOA_CRH	&= (0X00200000);
+	GPIOA_CRH	|= (0X00200000);
     /* Loop forever */
 	while(1)
 	{
